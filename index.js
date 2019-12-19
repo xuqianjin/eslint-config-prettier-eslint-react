fe4774c39a05f46dae1071d30c1cd7f28b044d90
xuqianjin <719900503@qq.com>
2019-12-19 19:14:20 +0800

    update

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 module.exports = {
-  extends: ["react-app","standard"],
+  extends: ["eslint-config-react-app", "eslint-config-standard"],
   rules: {
-    "indent": ["error", 2, { "SwitchCase": 1 }],
-    "space-before-function-paren": [ "error", {
-        "anonymous": "always",
-        "named": "never",
-        "asyncArrow": "always",
-    }],
+    indent: ["error", 2, { SwitchCase: 1 }],
+    "space-before-function-paren": [
+      "error",
+      {
+        anonymous: "always",
+        named: "never",
+        asyncArrow: "always"
+      }
+    ],
     "global-require": 0,
     "no-console": 1,
     "no-use-before-define": 0,
-    "radix": 0,
+    radix: 0,
     "eol-last": ["error", "always"],
-    "semi": [2, "always"],
-    "quotes": ["error", "single"],
+    semi: [2, "always"],
+    quotes: ["error", "single"],
     "comma-dangle": ["error", "always-multiline"],
     "react/destructuring-assignment": 0,
     "react/jsx-filename-extension": 0,
