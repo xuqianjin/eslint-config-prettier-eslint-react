f15bd73d3a4055e5fe27d88576a776a0287e0e8c
xuqianjin <719900503@qq.com>
2020-03-27 13:04:02 +0800

    upgrade

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 module.exports = {
   extends: ["react-app", "standard"],
   rules: {
-    "jsx-a11y/alt-text": 0
+    "jsx-a11y/alt-text": 0,
+    "import/order": [
+      "error",
+      { groups: [["builtin", "external", "internal"]] }
+    ],
+    "import/newline-after-import": 2
   }
 };
