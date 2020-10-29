eb4f33a6ad065ef890f0ced1624e54f75cbe2a3f
xuqianjin <719900503@qq.com>
2020-10-29 15:03:35 +0800

    update

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 module.exports = {
-  extends: ["react-app", "standard"],
+  extends: [
+    'eslint:recommended',
+    'google',
+    'plugin:react/recommended',
+    'plugin:promise/recommended',
+    'plugin:react-hooks/recommended',
+  ],
   rules: {
-    "jsx-a11y/alt-text": 0,
-    "import/order": [
-      "error",
-      { groups: [["builtin", "external", "internal"]] }
-    ],
-    "import/newline-after-import": 2
-  }
+    'require-jsdoc': 'off',
+    'linebreak-style': 'off',
+    'import/newline-after-import': 2,
+    'import/order': ['error', {groups: [['builtin', 'external', 'internal']]}],
+    'promise/catch-or-return': 'off',
+    'indent': 'off',
+    'react/prop-types': 'warn',
+  },
+  plugins: ['import', 'react-native'],
+  env: {
+  },
 };
